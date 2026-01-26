906f384cfc03869fac405acb0fd57323df1bfdd2
l-cnas <lin.narbutas@gmail.com>
2026-01-26 11:15:33 +0200

    update

---
+++ Workshop/Workshop-2026/07 - 1.26.2026/index.php
@@ -0,0 +1,68 @@
+<?php
+
+$data = [
+    'page_title' => "testinė užduotis nr. 6",    // Turi būti atvaizduota visada pirma didžioji raidė
+    'menu' => [
+        [
+            'title' => 'Namai',
+            'link' => '/',
+            'url' => '???',
+        ],
+        [
+            'title' => 'Rytas',
+            'link' => '/rytas.php',
+            'url' => '???',
+        ],
+        [
+            'title' => 'Žalgiris',
+            'link' => '/zalia_balta.php',
+            'url' => '???',
+        ],
+    ],
+    'content' => [
+        [
+            'title' => 'Buvusios Žalgirios rungtynės su Rytu',
+            'short_content' => 'Kas laimės nežino niekas',
+            'img_url' => '???',
+            'url' => '???',
+        ],
+        [
+            'title' => 'Suduvos pergalė prieš Lietkabelį, ar tikėtina?',
+            'short_content' => '???',
+            'img_url' => '???',
+            'url' => '???',
+        ],
+        [
+            'title' => 'A.Sabonio krikštas',
+            'short_content' => '???',
+            'img_url' => '???',
+            'url' => '???',
+        ],
+        [
+            'title' => '???',
+            'short_content' => '???',
+            'img_url' => '???',
+            'url' => '???',
+        ],
+    ],
+    'ads' => [
+        [
+            'buyerId' => 259,
+            'title' => '???',
+            'desc' => '???',
+            'img_alt' => 'Akropolio reklama',
+            'img_url' => '???',
+            'url' => '???',
+            'time_length' => 10,
+        ],
+        [
+            'buyerId' => 259,
+            'title' => '???',
+            'desc' => '???',
+            'img_alt' => '???',
+            'img_url' => '???',
+            'url' => '???',
+            'time_length' => '???',
+        ]
+    ]
+];
