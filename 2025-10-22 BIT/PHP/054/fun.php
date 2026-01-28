59d567e7bcfc6401d9c21149b4e3c2d694863451
l-cnas <lin.narbutas@gmail.com>
2026-01-28 13:34:51 +0200

    update

--- 2025-10-22 BIT/PHP/054/fun.php
+++ 2025-10-22 BIT/PHP/054/fun.php
@@ -36,7 +36,8 @@ echo '<pre>';
 print_r($kreivasMasyvas);
 
 
-function countArraySum($arr) {
+function countArraySum($arr) 
+{
     $sum = 0;
     foreach ($arr as $item) {
         if (is_array($item)) {
@@ -70,3 +71,71 @@ $zaliasPuslapis = [
 ];
 
 print_r($zaliasPuslapis);
+
+function labas()
+{
+    static $x = 0; // statinė vidinio funkcijos kintamojo deklaracija
+
+    $x = $x + 1;
+
+    echo "<br>X: $x";
+}
+
+labas();
+labas();
+labas();
+
+ // $arr kintamasis turi būti array arba int tipo
+function darbasSuMasyvais(array|int $arr) : ?int // grąžint turi int araba NULL (?)
+{
+    if (is_int($arr)) {
+
+        if ($arr === 0) {
+            return null;
+        }
+
+        return $arr;
+    }    
+
+    return count($arr);
+}
+
+$mas = [1, 3, 7];
+
+
+$rez = darbasSuMasyvais(0);
+
+echo "<h2>rezultatas: $rez</h2>";
+
+
+// Anoniminė
+
+$anomFun = function() {
+    echo '<h2>FUN FUN</h2>';
+};
+
+$anomFun();
+
+
+$colors = ['Orange', 'Blue', 'Green', 'Yellow', 'Red'];
+
+$raide = 2;
+
+
+usort($colors, function($a, $b) use ($raide) { // su use $raide yra perduodama į funkcijos vidų
+    return $a[$raide] <=> $b[$raide];
+});
+
+$raide = 1;
+
+// Arrow funkcija
+// Gali būti tik 1 eilutė
+// Kintamieji į vidų iš išorės patenka
+
+usort($colors, fn($a, $b) => $a[$raide] <=> $b[$raide]);
+
+
+
+echo '<br>';
+
+print_r($colors);
